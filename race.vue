8c255b7236f920fbe0dec6118e22e9d7b920abc0
Nick Cruz <nickcruz920@gmail.com>
2023-03-09 16:06:36 -0500

    Added template

---
+++ race.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+  
+    <div v-for="entry in entries">
+      <h2>Marathon List:<br>{{ entry.ID }} {{ entry.name }}</h2>
+    </div>
+    
+    <button @click="recordEntries">Click For Race Entries</button>
+    
+    <div class="output"></div>
+    
+  </div>
+</template>
